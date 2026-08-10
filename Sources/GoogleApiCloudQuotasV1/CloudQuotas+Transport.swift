8aeaf8d20c582a7bffcf4333b2d0e1c86d095e13
Carlos O'Ryan <coryan@google.com>
2026-08-10 19:39:36 -0400

    chore: update librarian circa 2026-08-10 (#305)
    This version of librarian:
    
    Generates code using `GoogleCloudGax._HTTPClient` and friends. As a
    reminder: this HTTP client is based on `AsyncHTTPClient` and is more
    suitable for server-side applications.
    
    Produces correct xref links for services and methods.

--- Sources/GoogleApiCloudQuotasV1/CloudQuotas+Transport.swift
+++ Sources/GoogleApiCloudQuotasV1/CloudQuotas+Transport.swift
@@ -19,15 +19,17 @@ import Foundation
   import FoundationNetworking
 #endif
 import GoogleCloudWkt
-import GoogleCloudGax
+@_spi(GoogleCloudInternal) import GoogleCloudGax
 
 extension Clients {
   class CloudQuotasTransport: CloudQuotasStub {
-    let inner: GoogleCloudGax.HTTPClient
+    let inner: GoogleCloudGax._HTTPClient
 
     public init(_ options: GoogleCloudGax.ClientOptions = .init()) throws {
-      self.inner = try GoogleCloudGax.HTTPClient(
-        from: options, withDefaultEndpoint: "https://cloudquotas.googleapis.com")
+      self.inner = try GoogleCloudGax._HTTPClient(
+        from: options,
+        withDefaultEndpoint: "https://cloudquotas.googleapis.com",
+      )
     }
 
     public func listQuotaInfos(
@@ -45,12 +47,12 @@ extension Clients {
       let encoder = GoogleCloudGax.QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.pageSize, prefix: "pageSize"))
       query.append(contentsOf: try encoder.encode(request.pageToken, prefix: "pageToken"))
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "GET"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.ListQuotaInfosResponse.self, from: data)
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.GET)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.ListQuotaInfosResponse.self, timeout: options.attemptTimeout
+      ).get()
     }
 
     public func getQuotaInfo(
@@ -65,12 +67,12 @@ extension Clients {
       let query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "GET"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.QuotaInfo.self, from: data)
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.GET)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.QuotaInfo.self, timeout: options.attemptTimeout
+      ).get()
     }
 
     public func listQuotaPreferences(
@@ -90,12 +92,12 @@ extension Clients {
       query.append(contentsOf: try encoder.encode(request.pageToken, prefix: "pageToken"))
       query.append(contentsOf: try encoder.encode(request.filter, prefix: "filter"))
       query.append(contentsOf: try encoder.encode(request.orderBy, prefix: "orderBy"))
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "GET"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.ListQuotaPreferencesResponse.self, from: data)
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.GET)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.ListQuotaPreferencesResponse.self, timeout: options.attemptTimeout
+      ).get()
     }
 
     public func getQuotaPreference(
@@ -110,12 +112,12 @@ extension Clients {
       let query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "GET"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.QuotaPreference.self, from: data)
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.GET)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.QuotaPreference.self, timeout: options.attemptTimeout
+      ).get()
     }
 
     public func createQuotaPreference(
@@ -135,16 +137,15 @@ extension Clients {
         contentsOf: try encoder.encode(request.quotaPreferenceId, prefix: "quotaPreferenceId"))
       query.append(
         contentsOf: try encoder.encode(request.ignoreSafetyChecks, prefix: "ignoreSafetyChecks"))
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "POST"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.POST)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
       if let body = request.quotaPreference {
-        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        req.httpBody = try JSONEncoder().encode(body)
+        req.setBody(data: try JSONEncoder().encode(body), ofContentType: "application/json")
       }
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.QuotaPreference.self, from: data)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.QuotaPreference.self, timeout: options.attemptTimeout
+      ).get()
     }
 
     public func updateQuotaPreference(
@@ -167,16 +168,15 @@ extension Clients {
       query.append(contentsOf: try encoder.encode(request.validateOnly, prefix: "validateOnly"))
       query.append(
         contentsOf: try encoder.encode(request.ignoreSafetyChecks, prefix: "ignoreSafetyChecks"))
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "PATCH"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.PATCH)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
       if let body = request.quotaPreference {
-        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        req.httpBody = try JSONEncoder().encode(body)
+        req.setBody(data: try JSONEncoder().encode(body), ofContentType: "application/json")
       }
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.QuotaPreference.self, from: data)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.QuotaPreference.self, timeout: options.attemptTimeout
+      ).get()
     }
   }
 }
--- Sources/GoogleApiCloudQuotasV1/QuotaAdjusterSettingsManager+Transport.swift
+++ Sources/GoogleApiCloudQuotasV1/QuotaAdjusterSettingsManager+Transport.swift
@@ -19,15 +19,17 @@ import Foundation
   import FoundationNetworking
 #endif
 import GoogleCloudWkt
-import GoogleCloudGax
+@_spi(GoogleCloudInternal) import GoogleCloudGax
 
 extension Clients {
   class QuotaAdjusterSettingsManagerTransport: QuotaAdjusterSettingsManagerStub {
-    let inner: GoogleCloudGax.HTTPClient
+    let inner: GoogleCloudGax._HTTPClient
 
     public init(_ options: GoogleCloudGax.ClientOptions = .init()) throws {
-      self.inner = try GoogleCloudGax.HTTPClient(
-        from: options, withDefaultEndpoint: "https://cloudquotas.googleapis.com")
+      self.inner = try GoogleCloudGax._HTTPClient(
+        from: options,
+        withDefaultEndpoint: "https://cloudquotas.googleapis.com",
+      )
     }
 
     public func updateQuotaAdjusterSettings(
@@ -48,16 +50,15 @@ extension Clients {
       let encoder = GoogleCloudGax.QueryParameterEncoder()
       query.append(contentsOf: try encoder.encode(request.updateMask, prefix: "updateMask"))
       query.append(contentsOf: try encoder.encode(request.validateOnly, prefix: "validateOnly"))
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "PATCH"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.PATCH)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
       if let body = request.quotaAdjusterSettings {
-        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        req.httpBody = try JSONEncoder().encode(body)
+        req.setBody(data: try JSONEncoder().encode(body), ofContentType: "application/json")
       }
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.QuotaAdjusterSettings.self, from: data)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.QuotaAdjusterSettings.self, timeout: options.attemptTimeout
+      ).get()
     }
 
     public func getQuotaAdjusterSettings(
@@ -72,12 +73,12 @@ extension Clients {
       let query = [
         URLQueryItem(name: "$alt", value: "json;enum-encoding=int")
       ]
-      var req = try await self.inner.Request(path: path, query: query)
-      req.httpMethod = "GET"
-      req.setValue(Clients.clientHeader, forHTTPHeaderField: "X-Goog-Api-Client")
-      let (data, _) = try await self.inner.rpc(for: req).get()
-      return try GoogleCloudWkt._ProtoJSONDecoder().decode(
-        GoogleApiCloudQuotasV1.QuotaAdjusterSettings.self, from: data)
+      var req = try await self.inner.newRequest(path: path, query: query)
+      req.setMethod(.GET)
+      req.addHeader(name: "X-Goog-Api-Client", value: Clients.clientHeader)
+      return try await req.rpc(
+        GoogleApiCloudQuotasV1.QuotaAdjusterSettings.self, timeout: options.attemptTimeout
+      ).get()
     }
   }
 }
