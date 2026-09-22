ac275ec8841ec6e492bb69185c0f08f796429516
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleApiCloudQuotasV1/QuotaAdjusterSettings.swift
+++ Sources/GoogleApiCloudQuotasV1/QuotaAdjusterSettings.swift
@@ -138,6 +138,12 @@ public struct QuotaAdjusterSettings: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// The enablement status of the quota adjuster.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum Enablement: Codable, Equatable, Sendable {
     /// The quota adjuster is in an unknown state.
     case unspecified
@@ -147,15 +153,21 @@ public struct QuotaAdjusterSettings: Codable, Equatable, GoogleWKT._AnyPackable,
     case disabled
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
--- Sources/GoogleApiCloudQuotasV1/QuotaInfo.swift
+++ Sources/GoogleApiCloudQuotasV1/QuotaInfo.swift
@@ -228,6 +228,12 @@ public struct QuotaInfo: Codable, Equatable, GoogleWKT._AnyPackable,
   }
 
   /// The enumeration of the types of a cloud resource container.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum ContainerType: Codable, Equatable, Sendable {
     /// Unspecified container type.
     case unspecified
@@ -239,15 +245,21 @@ public struct QuotaInfo: Codable, Equatable, GoogleWKT._AnyPackable,
     case organization
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
